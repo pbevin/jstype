4e2f25514aeb044106f7ac2dca33583b218713ff
Pete Bevin <pete@petebevin.com>
2015-05-29 10:34:28 -0400

    Make Math constants read-only

--- src/Runtime.hs
+++ src/Runtime.hs
@@ -246,34 +246,7 @@ createGlobalThis = do
                     >>= objSetPrototype functionPrototype
                     >>= addOwnProperty "prototype" (VObj datePrototype)
 
-  math <- newObject >>= addOwnProperty "PI" (VNum $ JSNum (pi :: Double))
-                    >>= addOwnProperty "E" (VNum $ JSNum (exp 1 :: Double))
-                    >>= addOwnProperty "LN2" (VNum $ JSNum (log 2 :: Double))
-                    >>= addOwnProperty "LN10" (VNum $ JSNum (log 10 :: Double))
-                    >>= addOwnProperty "LOG10E" (VNum $ JSNum $ 1 / (log 10 :: Double))
-                    >>= addOwnProperty "LOG2E" (VNum $ JSNum $ 1 / (log 2 :: Double))
-                    >>= addOwnProperty "abs" (VNative $ mathFunc abs)
-                    >>= addOwnProperty "log" (VNative $ mathFunc log)
-                    >>= addOwnProperty "exp" (VNative $ mathFunc exp)
-                    >>= addOwnProperty "sin" (VNative $ mathFunc sin)
-                    >>= addOwnProperty "cos" (VNative $ mathFunc cos)
-                    >>= addOwnProperty "tan" (VNative $ mathFunc tan)
-                    >>= addOwnProperty "asin" (VNative $ mathFunc asin)
-                    >>= addOwnProperty "acos" (VNative $ mathFunc acos)
-                    >>= addOwnProperty "atan" (VNative $ mathFunc atan)
-                    >>= addOwnProperty "sqrt" (VNative $ mathFunc sqrt)
-                    >>= addOwnProperty "ceil" (VNative $ mathFunc $ fromIntegral . ceiling)
-                    >>= addOwnProperty "round" (VNative $ mathFunc $ fromIntegral . round)
-                    >>= addOwnProperty "floor" (VNative $ mathFunc $ fromIntegral . floor)
-                    >>= addOwnProperty "trunc" (VNative $ mathFunc $ fromIntegral . truncate)
-
-                    >>= addOwnProperty "max" (VNative $ mathMaxFunc maximum)
-                    >>= addOwnProperty "min" (VNative $ mathMaxFunc minimum)
-
-                    >>= addOwnProperty "pow" (VNative $ mathFunc2 pow)
-                    >>= addOwnProperty "atan2" (VNative $ mathFunc2 atan2)
-                    >>= addOwnProperty "hypot" (VNative $ mathFunc2 hypot)
-
+  math <- mathObject
   json <- newObject >>= addOwnProperty "stringify" (VNative jsonStringify)
 
   newObject >>= addOwnProperty "escape" (VNative objEscape)
@@ -297,6 +270,47 @@ createGlobalThis = do
             >>= addOwnProperty "Infinity" (VNum $ 1 / 0)
             >>= addOwnProperty "NaN" (VNum $ jsNaN)
             >>= addOwnProperty "isNaN" (VNative objIsNaN)
+
+
+mathObject :: Runtime (Shared JSObj)
+mathObject = do
+  newObject >>= addReadOnlyConstants mathConstants
+            >>= addOwnProperty "abs" (VNative $ mathFunc abs)
+            >>= addOwnProperty "log" (VNative $ mathFunc log)
+            >>= addOwnProperty "exp" (VNative $ mathFunc exp)
+            >>= addOwnProperty "sin" (VNative $ mathFunc sin)
+            >>= addOwnProperty "cos" (VNative $ mathFunc cos)
+            >>= addOwnProperty "tan" (VNative $ mathFunc tan)
+            >>= addOwnProperty "asin" (VNative $ mathFunc asin)
+            >>= addOwnProperty "acos" (VNative $ mathFunc acos)
+            >>= addOwnProperty "atan" (VNative $ mathFunc atan)
+            >>= addOwnProperty "sqrt" (VNative $ mathFunc sqrt)
+            >>= addOwnProperty "ceil" (VNative $ mathFunc $ fromIntegral . ceiling)
+            >>= addOwnProperty "round" (VNative $ mathFunc $ fromIntegral . round)
+            >>= addOwnProperty "floor" (VNative $ mathFunc $ fromIntegral . floor)
+            >>= addOwnProperty "trunc" (VNative $ mathFunc $ fromIntegral . truncate)
+
+            >>= addOwnProperty "max" (VNative $ mathMaxFunc maximum)
+            >>= addOwnProperty "min" (VNative $ mathMaxFunc minimum)
+
+            >>= addOwnProperty "pow" (VNative $ mathFunc2 pow)
+            >>= addOwnProperty "atan2" (VNative $ mathFunc2 atan2)
+            >>= addOwnProperty "hypot" (VNative $ mathFunc2 hypot)
+
+mathConstants :: [(String, Double)]
+mathConstants = [ ("PI", pi),
+                  ("E", exp 1),
+                  ("LN2", log 2),
+                  ("LN10", log 10),
+                  ("LOG10E", 1 / log 10),
+                  ("LOG2E", 1 / log 2) ]
+
+addReadOnlyConstants :: [(String, Double)] -> Shared JSObj -> Runtime (Shared JSObj)
+addReadOnlyConstants xs obj = do
+  forM (map toReadOnlyPropDesc xs) $ \(name, desc) -> do
+    objDefineOwnProperty name desc False obj
+  return obj
+  where toReadOnlyPropDesc (name, value) = (name, DataPD (VNum $ JSNum value) False False False)
 
 
 errorType :: String -> JSVal -> Runtime (Shared JSObj)
