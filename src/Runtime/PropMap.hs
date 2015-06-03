affec301d53e40c5726a900dc5b9ae25e0fbe777
Pete Bevin <pete@petebevin.com>
2015-06-02 21:05:28 -0400

    Add deletable flag to PropMap entries

--- src/Runtime/PropMap.hs
+++ src/Runtime/PropMap.hs
@@ -2,7 +2,7 @@ module Runtime.PropMap where
 
 import qualified Data.Map as M
 
-newtype PropMap k a = PropMap (M.Map k a) deriving (Show, Eq)
+newtype PropMap k a = PropMap (M.Map k (a, Bool)) deriving (Show, Eq)
 
 emptyPropMap :: PropMap k a
 emptyPropMap = PropMap M.empty
@@ -11,19 +11,21 @@ propMapMember :: Ord k => k -> PropMap k a -> Bool
 propMapMember k (PropMap m) = M.member k m
 
 propMapLookup :: Ord k => k -> PropMap k a -> Maybe a
-propMapLookup k (PropMap m) = M.lookup k m
+propMapLookup k (PropMap m) = fst <$> M.lookup k m
 
 propMapInsert :: Ord k => k -> a -> PropMap k a -> PropMap k a
-propMapInsert k a (PropMap m) = PropMap $ M.insert k a m
+propMapInsert k a (PropMap m) = PropMap $ M.insert k (a, False) m
 
 propMapDelete :: Ord k => k -> PropMap k a -> PropMap k a
 propMapDelete k (PropMap m) = PropMap $ M.delete k m
 
 propMapToList :: PropMap k a -> [(k, a)]
-propMapToList (PropMap m) = M.toList m
+propMapToList (PropMap m) = map unwrap (M.toList m)
+  where unwrap (k, (a, _)) = (k, a)
 
 propMapFromList :: Ord k => [(k, a)] -> PropMap k a
-propMapFromList = PropMap . M.fromList
+propMapFromList = PropMap . M.fromList . map f
+  where f (k,a) = (k, (a, False))
 
 propMapKeys :: PropMap k a -> [k]
 propMapKeys (PropMap m) = M.keys m
