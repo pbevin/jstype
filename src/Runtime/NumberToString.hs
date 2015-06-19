51346c2d7a16639dfc2519d54e3fb88a1c5c424d
Pete Bevin <pete@petebevin.com>
2015-06-19 07:27:46 -0400

    Refactor NumberToString to fix compiler warnings

--- src/Runtime/NumberToString.hs
+++ src/Runtime/NumberToString.hs
@@ -3,6 +3,7 @@ module Runtime.NumberToString where
 import Control.Exception
 import Data.Char
 import Debug.Trace
+import Prelude hiding (exponent)
 
 type Base = Integer
 type Precision = Int
@@ -23,13 +24,13 @@ n2s f
   | k <= n && n <= 21   = d ++ replicate (n-k) '0'
   | 0 <  n && n <= 21   = take n d ++ "." ++ drop n d
   | -6 < n && n <= 0    = "0." ++ replicate (-n) '0' ++ d
-  | k == 1              = d ++ "e" ++ exponent n
-  | otherwise           = take 1 d ++ "." ++ drop 1 d ++ "e" ++ exponent n
+  | k == 1              = d ++ "e" ++ exponent
+  | otherwise           = take 1 d ++ "." ++ drop 1 d ++ "e" ++ exponent
   where (n, k, s) = splitNumber f
         d = map (intToDigit . fromIntegral) s
-        exponent n
-          | n-1 > 0 = "+" ++ show (n-1)
-          | n-1 < 0 = "-" ++ show (1-n)
+        exponent
+          | n-1 > 0   = "+" ++ show (n-1)
+          | otherwise = "-" ++ show (1-n)
 
 type FPPState = (Int, Int, Integer, Integer, Integer, Integer)
 
@@ -41,33 +42,33 @@ dissectFloat d = (b, e, f, p)
         (f, e') = decodeFloat d
 
 splitNumber :: Double -> (Int, Int, [Integer])
-splitNumber v = fpp2 $ fixup (0, 0, r, s, mm, mp)
+splitNumber v = fpp2 . fixup $ start
   where
-      (b, e, f, p) = dissectFloat v
-      r  = shiftb f $ max 0 $ e-p
-      s  = shiftb 1 $ max 0 $ p-e
-      mm = shiftb 1 $ max 0 $ e-p
-      mp = mm
+      start :: FPPState
+      start = if f == shiftb 1 (p-1)
+              then (0, 0, r*b, s*b, mm, mp*b)
+              else (0, 0, r, s, mm, mp)
 
-      shiftb :: Integer -> Int -> Integer
-      shiftb x n = foldr (.) id (replicate n (*b)) x
+        where
+          (b, e, f, p) = dissectFloat v
+          r  = shiftb f $ max 0 $ e-p
+          s  = shiftb 1 $ max 0 $ p-e
+          mm = shiftb 1 $ max 0 $ e-p
+          mp = mm
+
+          shiftb :: Integer -> Int -> Integer
+          shiftb x n = foldr (.) id (replicate n (*b)) x
 
       fixup :: FPPState -> FPPState
       fixup (n,k,r,s,mm,mp) =
-        if f == shiftb 1 (p-1)
-        then fixup' (n, k, r*b, s*b, mm, mp*b)
-        else fixup' (n, k, r, s, mm, mp)
+        if 10*r < s + 10
+        then fixup (n-1, k, r*10, s, mm*10, mp*10)
+        else fixup' (n,k,r,s,mm,mp)
 
       fixup' :: FPPState -> FPPState
       fixup' (n,k,r,s,mm,mp) =
-        if 10*r < s + 10
-        then fixup' (n-1, k, r*10, s, mm*10, mp*10)
-        else fixup'' (n,k,r,s,mm,mp)
-
-      fixup'' :: FPPState -> FPPState
-      fixup'' (n,k,r,s,mm,mp) =
         if 2*r + mp >= 2*s
-        then fixup'' (n+1, k, r, s*10, mm, mp)
+        then fixup' (n+1, k, r, s*10, mm, mp)
         else (n,k,r,s,mm,mp)
 
       fpp2 :: FPPState -> (Int, Int, [Integer])
